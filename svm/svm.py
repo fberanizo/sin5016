8ac339d53f86cf985c7eb29d9da11a77cd3865e1
Fabio Lopes <fabiol@cpqd.com.br>
2016-11-30 17:30:31 -0200

    Implementação parcial do SMO

--- svm/svm.py
+++ svm/svm.py
@@ -11,8 +11,8 @@ class SVM(BaseEstimator, ClassifierMixin):
         self.C = C
         self.kernel = kernel
         self.gamma = gamma
-        self.eps = 1e-5
-        self.tolerance = 1e-5 # ksi
+        self.eps = 1e-3
+        self.tol = 1e-3 # ksi
 
     def fit(self, X, y):
         """Treina o SVM utilizando o algoritmo SMO. 
@@ -35,64 +35,121 @@ class SVM(BaseEstimator, ClassifierMixin):
         # Inicializa multiplicadores de Lagrange para cada amostra
         self.alph = numpy.zeros((X.shape[0],))
 
-        # A cada iteração seleciona duas amostras de classes diferentes
-        # que violem condições de KKT por mais que a tolerância 
-        # e que sejam 'non-bound' (vetores de margem ou de erro)
+        samples_violating_kkt_conditions = 0
+        loop_all_samples = True
 
-        # Seleciona amostra da classe "1"
-        idx1 = self.select_sample_1()
+        # Alterna entre todas amostras e amostras non-bound (0 < alpha < C)
+        # até que todas amostras obedeçam as condições de KKT por um epsilon  
+        while samples_violating_kkt_conditions > 0 or loop_all_samples:
+            
+            if loop_all_samples:
+                partition = numpy.where(self.alph)
+            else:
+                partition = numpy.where(numpy.logical_and(self.alph > 0, self.alph < self.C))
 
-        # Seleciona amostra da classe "2"
-        idx2 = self.select_sample_2(idx1)
+            for sample in partition[0]:
+                samples_violating_kkt_conditions += self.examine_sample(sample)
 
-        print(idx1)
-        print(idx2)
+            if loop_all_samples:
+                loop_all_samples = False
+            elif samples_violating_kkt_conditions == 0:
+                loop_all_samples = True
 
         return self
 
-    def f(self, x):
-        """Calcula o valor de f(x) para kernel linear."""
-        fx = sum(numpy.multiply(numpy.multiply(self.y, self.alph), numpy.dot(self.X, x)) + self.tolerance)
-        return -1 if fx < 0 else 1
+    def examine_sample(self, sample1):
+        """Retorna 1 caso otimização foi possível, do contrário retorna 0."""
+        y1 = self.y[sample1,:]
+        alph1 = self.alph[sample1]
+        E1 = self.f(self.X[sample1,:]) - y2 # TODO: buscar E1 na cache
+        r2 = E2*y2
+        if (r2 < -self.tol and alph2 < C) or (r2 > self.tol and alph2 > 0):
+            # Há 3 estratégias para selecionar a segunda amostra:
+            # 1) Escolhe a segunda amostra que maximize o passe de otimização, aproximado por |E1-E2|
 
-    def select_sample_1(self):
-        idx1 = -1
-        for idx in self.partition_1[0]:
-            x = self.X[idx,:]; y = 1; alph = self.alph[idx]
-            fx1 = self.f(x.T)
-            kkt = fx1 * y - 1
+            # 2) Escolhe amostra aleatória dentre as non-bound (0 < alpha < C)
 
-            if ((alph == 0 and kkt <= 0) or
-                (alph == self.C and kkt >= 0) or
-                (0 < alph < self.C and abs(kkt) < self.eps)):
-                idx1 = idx
-                break
-        return idx1
+            # 3) Escolhe amostra aleatória dentre todas
+            return 1
 
-    def select_sample_2(self, idx1):
-        idx2 = -1; fx1 = self.f(self.X[idx1,:].T); fx2 = 0; tmax = -1
-        for idx in self.partition_2[0]:
-            x = self.X[idx,:]; y = -1; alph = self.alph[idx]
-            fx2 = self.f(x.T)
-            kkt = fx2 * y - 1
+        return 0
 
-            if ((alph == 0 and kkt <= 0) or
-                (alph == self.C and kkt >= 0) or
-                (0 < alph < self.C and abs(kkt) < self.eps)):
+    def optimize_samples(self, sample1, sample2):
+        """"""
+        if sample1 == sample2:
+            return 0
 
-                temp = abs(fx2 - fx1)
+        y1 = self.y[sample1,:]
+        E1 = self.f(self.X[sample1,:]) - y2 # TODO: buscar E1 na cache
+        alph1 = self.alph[sample1]
+        alph2 = self.alph[sample2]
+        s = y1*y2
+        lower_bound, upper_bound = self.compute_bounds()
+        if lower_bound == upper_bound:
+            return 0
 
-                if temp > tmax:
-                    tmax = temp
-                    idx2 = idx
-        return idx2
+        k11 = self.kernel_func(self.X[sample1,:], self.X[sample1,:])
+        k12 = self.kernel_func(self.X[sample1,:], self.X[sample2,:])
+        k22 = self.kernel_func(self.X[sample2,:], self.X[sample2,:])
+        eta = k11 + k22 -2*k12
 
+        if eta > 0:
+            a2 = alph2 + y2*(E1-E2)/eta
+            if a2 < lower_bound:
+                a2 = lower_bound
+            elif a2 > upper_bound:
+                a2 = upper_bound
+        else:
+            # objective function at a2=lower_bound
+            self.alph[sample2] = lower_bound
+            l_fx = self.f(self.X[sample2])
+
+            # objective function at a2=upper_bound
+            self.alph[sample2] = upper_bound
+            u_fx = self.f(self.X[sample2])
+
+            if l_fx < u_fx - self.eps:
+                a2 = lower_bound
+            elif l_fx > u_fx + self.eps:
+                a2 = upper_bound
+            else:
+                a2 = alph2
+
+        if numpy.abs(a2-alph2) < self.eps*(a2 + alph2 + self.eps):
+            return 0
+
+        a1 = alph1 + s*(alph2 - a2)
+        # Update threshold to reflect change in Lagrange multipliers
+        # Update weight vector to reflect change in a1 & a2, if SVM is linear
+        # Update error cache using new Lagrange multipliers
+        
+        self.alph[sample1] = a1
+        self.alph[sample2] = a2
+        return 1
+
+    def f(self, X):
+        """Calcula a saída do classificador para uma dada amostra X."""
+        return sum(numpy.multiply(numpy.multiply(self.y, self.alph), numpy.apply_along_axis(self.kernel_func, 1, self.X, X)) - self.b())
+
+    def b(self):
+        """Calcula o parâmetro b."""
+        return 0.0
+
+    def kernel_func(self, Xj, X):
+        """Calcula saída da função kernel."""
+        if self.kernel == 'linear':
+            return numpy.dot(Xj, X)
+        else:
+            raise Exception('Kernel %s not implemented' % self.kernel)
+
+    def compute_bounds(self):
+        return lower_bound, upper_bound
 
     def predict(self, X):
         """Rotula amostras utilizando o SVM previamente treinado."""
         y = []
         for x_row in X:
-            y_row = self.classes[0] if self.f(x_row) == 1 else self.classes[1]
+            y_row = self.classes[0] if self.f(x_row) >= 1-self.eps else self.classes[1]
             y.append(y_row)
 
         y = numpy.asarray(y)
