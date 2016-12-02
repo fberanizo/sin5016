2aa87fc1034cacf169a47bb9c2f4309fe77ff10e
Fabio Lopes <fabiol@cpqd.com.br>
2016-12-02 16:20:39 -0200

    Adiciona kernel RBF

--- svm/svm.py
+++ svm/svm.py
@@ -3,6 +3,7 @@
 import numpy, random, time
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.preprocessing import normalize
+from sklearn.metrics import accuracy_score
 
 class SVM(BaseEstimator, ClassifierMixin):
     """Classe que implementa um SVM."""
@@ -61,23 +62,23 @@ class SVM(BaseEstimator, ClassifierMixin):
 
         return self
 
-    def examine_sample(self, sample2):
+    def examine_sample(self, sample1):
         """Retorna 1 caso otimização foi possível, do contrário retorna 0."""
-        y2 = self.y[sample2]
-        alph2 = self.alph[sample2]
-        E2 = self.E(sample2)
-        r2 = E2*y2
+        y1 = self.y[sample1]
+        alph1 = self.alph[sample1]
+        E1 = self.E(sample1, recalc=(alph1 == 0 or alph1 == self.C))
+        r1 = E1*y1
         #print("examine_sample %d, y = %d, alph2 = %f" % (sample2, y2, alph2))
 
-        if (r2 < -self.tol and alph2 < self.C) or (r2 > self.tol and alph2 > 0):
+        if (r1 < -self.tol and alph1 < self.C) or (r1 > self.tol and alph1 > 0):
             # Há 3 estratégias para selecionar a segunda amostra:
             # 1) Escolhe a segunda amostra que maximize o passe de otimização, aproximado por |E1-E2|
 
             best_sample2 = -1
             best_optimization_step = -1
             non_bound_partition = numpy.where(numpy.logical_and(self.alph > 0, self.alph < self.C))[0]
-            for sample1 in non_bound_partition:
-                E1 = self.E(sample1)
+            for sample2 in non_bound_partition:
+                E1 = self.E(sample2)
                 step = abs(E1-E2)
                 if step > best_optimization_step:
                     best_optimization_step = step
@@ -91,7 +92,7 @@ class SVM(BaseEstimator, ClassifierMixin):
             # 2) Escolhe amostra aleatória dentre as non-bound (0 < alpha < C)
             sample_list = list(non_bound_partition)
             random.shuffle(sample_list)
-            for sample1 in sample_list:
+            for sample2 in sample_list:
                 print("Otimizando amostras %d e %d pela estratégia 2" % (sample1, sample2))
                 if self.optimize_samples(sample1, sample2):
                     return 1
@@ -99,7 +100,7 @@ class SVM(BaseEstimator, ClassifierMixin):
             # 3) Escolhe amostra aleatória dentre todas
             sample_list = list(range(self.alph.size))
             random.shuffle(sample_list)
-            for sample1 in sample_list:
+            for sample2 in sample_list:
                 print("Otimizando amostras %d e %d pela estratégia 3" % (sample1, sample2))
                 if self.optimize_samples(sample1, sample2):
                     return 1
@@ -128,24 +129,21 @@ class SVM(BaseEstimator, ClassifierMixin):
         k22 = self.kernel_func(self.X[sample2,:], self.X[sample2,:])
         eta = k11 + k22 -2*k12
 
-        if eta > 0:
-            a2 = alph2 + y2*(E1-E2)/eta
+        if eta < 0:
+            a2 = alph2 + y2*(E2-E1)/eta
             if a2 < lower_bound:
                 a2 = lower_bound
             elif a2 > upper_bound:
                 a2 = upper_bound
         else:
-            # objective function at a2=lower_bound
-            self.alph[sample2] = lower_bound
-            l_fx = self.f(self.X[sample2])
+            c1 = eta/2
+            c2 = y2 * (E1-E2)- eta * alph2
+            l_fx = c1 * lower_bound * lower_bound + c2 * lower_bound
+            u_fx = c1 * upper_bound * upper_bound + c2 * upper_bound
 
-            # objective function at a2=upper_bound
-            self.alph[sample2] = upper_bound
-            u_fx = self.f(self.X[sample2])
-
-            if l_fx < u_fx - self.eps:
+            if l_fx > u_fx + self.eps:
                 a2 = lower_bound
-            elif l_fx > u_fx + self.eps:
+            elif l_fx < u_fx - self.eps:
                 a2 = upper_bound
             else:
                 a2 = alph2
@@ -153,21 +151,35 @@ class SVM(BaseEstimator, ClassifierMixin):
         if abs(a2-alph2) < self.eps*(a2 + alph2 + self.eps):
             return False
 
-        a1 = alph1 + s*(alph2 - a2)
+        a1 = alph1 - s*(a2-alph2)
+        if a1 < 0:
+            a2 += s * a1
+            a1 = 0
+        elif a1 > self.C:
+            t = a1 - self.C
+            a2 += s * t
+            a1 = C
         
         # Atualiza o parâmetro b
-        self.b = self.threshold(sample1, sample2, a1, a2)
+        self.b, self.delta_b = self.threshold(sample1, sample2, a1, a2, k11, k12, k22)
 
         # TODO: Update weight vector to reflect change in a1 & a2, if SVM is linear
+
+        # Atualiza cache de erro utilizando novos a1 e a2
+        non_bound_partition = numpy.where(numpy.logical_and(self.alph > 0, self.alph < self.C))[0]
+        t1 = y1 * (a1 - alph1)
+        t2 = y2 * (a2 - alph2)
+        for sample in non_bound_partition:
+            self.error_cache[sample] = t1 * self.kernel_func(sample1, sample) + t2 * self.kernel_func(sample2, sample) - self.delta_b
+
+        self.error_cache[sample1] = 0
+        self.error_cache[sample2] = 0
         
         # Atualiza valores de alpha1 e alpha2
         self.alph[sample1] = a1
         self.alph[sample2] = a2
 
-        # Atualiza cache de erro utilizando novos a1 e a2
-        self.E(sample1, recalc=True)
-        self.E(sample2, recalc=True)
-
+        print("accuracy %f" % self.accuracy())
         time.sleep(5)
         return True
 
@@ -175,33 +187,39 @@ class SVM(BaseEstimator, ClassifierMixin):
         """Calcula a saída do classificador para uma dada amostra X."""
         return sum(numpy.multiply(numpy.multiply(self.y, self.alph), numpy.apply_along_axis(self.kernel_func, 1, self.X, X)) - self.b)
 
-    def threshold(self, sample1, sample2, a1, a2):
+    def threshold(self, sample1, sample2, a1, a2, k11, k12, k22):
         """Calcula o parâmetro b."""
+
         y1 = self.y[sample1]
         X1 = self.X[sample1,:]
         alpha1 = self.alph[sample1]
         E1 = self.E(sample1)
+        
         y2 = self.y[sample2]
         X2 = self.X[sample2,:]
         alpha2 = self.alph[sample2]
         E2 = self.E(sample2)
-        
-        b1 = E1 + y1*(a1 - alpha1)*self.kernel_func(X1, X1) + y2*(a2 - alpha2)*self.kernel_func(X1, X2) + self.b
 
         if 0 < a1 < self.C:
-            return b1
+            bnew = self.b + E1 + y1 * (a1 - alpha1) * k11 + y2 * (a2 - alpha2) * k12
+        else:
+            if 0 < a2 < self.C:
+                bnew = self.b + E2 + y1 * (a1 - alpha1) * k12 + y2 * (a2 - alpha2) * k22
+            else:
+                b1 = self.b + E1 + y1 * (a1 - alpha1) * k11 + y2 * (a2 - alpha2) * k12
+                b2 = self.b + E2 + y1 * (a1 - alpha1) * k12 + y2 * (a2 - alpha2) * k22
+                bnew = (b1 + b2) / 2
 
-        b2 = E2 + y1*(a1 - alpha1)*self.kernel_func(X1, X2) + y2*(a2 - alpha2)*self.kernel_func(X2, X2) + self.b
-
-        if 0 < a2 < self.C:
-            return b2
-
-        return numpy.mean([b1, b2])
+        delta_b = bnew - self.b
+        return bnew, delta_b
 
     def kernel_func(self, Xj, X):
         """Calcula saída da função kernel."""
         if self.kernel == 'linear':
-            return numpy.dot(Xj, X)
+            return numpy.dot(Xj.T, X)
+        elif self.kernel == 'rbf':
+            s = numpy.dot(Xj.T, Xj) + numpy.dot(X.T, X) - 2*numpy.dot(Xj.T, X)
+            return numpy.exp(-s/2)
         else:
             raise Exception('Kernel %s not implemented' % self.kernel)
 
@@ -235,3 +253,9 @@ class SVM(BaseEstimator, ClassifierMixin):
 
         y = numpy.asarray(y)
         return y
+
+    def accuracy(self):
+        y_pred = numpy.apply_along_axis(self.f, 1, self.X)
+        y_pred = list(map(int, numpy.sign(y_pred)))
+        func = lambda prediction: self.classes[0] if prediction == 1 else self.classes[1]
+        return accuracy_score(list(map(func, self.y)), list(map(func, y_pred)))
